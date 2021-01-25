9d1ce5d96ce92cc8ab01d926dc8688d8ceb327f4
Brian Richardson <brian@cubik.ca>
2021-01-24 21:40:53 -0700

    WIP, cleanup (#1)
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * fix rebase

---
+++ Tests/Bookstore.Entities.Book.Tests/BookTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Bookstore.Entities.Book.Tests
+{
+    public class BookTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestFind()
+        {
+            Assert.Pass();
+        }
+    }
+}
